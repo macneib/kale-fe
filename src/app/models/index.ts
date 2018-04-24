731b239931fe15576b3cc468a72d607f902e7949
Blair MacNeil <blair@skooli.com>
2018-04-24 11:25:09 -0400

    added enums

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,8 +1,10 @@
+export * from './connectivityStatus.enum';
+export * from './exchange.enum';
+export * from './gatewayType.enum';
 export * from './side.enum';
 export * from './orderType.enum';
 export * from './timeInForce.enum';
 export * from './orderStatus.enum';
 export * from './liquidity.enum';
 export * from './MarketDataFlag.enum';
-
-export * from 'OrderSource.enum';
+export * from './OrderSource.enum';
